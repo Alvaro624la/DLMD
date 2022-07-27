0c105690452133c60ad0af8e33545d8fdd23ff0c
Alvaro624la <alvarodepradotrabajo@gmail.com>
2022-07-27 11:10:51 +0200

    feat: New emojis that react to your vital needs fix: CSS responsive problems

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const DATA = {
     person:{
+        fullLiving: './src/fullLiving.svg',
         happy: './src/happy.svg',
         neutral: './src/neutral.svg',
         sad:'./src/sad.svg',
         hungry: './src/hungry.svg',
         thirsty: './src/thirsty.svg',
-        sleep: './src/sleep.svg'
+        sleep: './src/sleep.svg',
+        dead: './src/dead.svg'
     },
     necessities:{
         food: './src/apple.svg',
@@ -23,38 +25,45 @@ let time = 0;
 let body = document.getElementById('body');
 let main = document.getElementById('main');
 let life = document.getElementById('life');
+let personState = document.getElementById('personState');
 let inventory = document.getElementById('inventory');
+let itemsCont = document.getElementById('itemsCont');
 
 //person container
-let personCont = document.createElement('div');
-personCont.className = 'life__personCont';
-life.appendChild(personCont);
+// let personCont = document.createElement('div');
+// personCont.className = 'life__personCont';
+// life.appendChild(personCont);
+let personCont = document.getElementById('personCont');
 
 //necessities container
-let necessitiesCont = document.createElement('div');
-necessitiesCont.className = 'life__necessitiesCont';
-life.appendChild(necessitiesCont);
+// let necessitiesCont = document.createElement('div');
+// necessitiesCont.className = 'life__necessitiesCont';
+// life.appendChild(necessitiesCont);
+let necessitiesCont = document.getElementById('div');
 
 //food container
-let foodCont = document.createElement('div');
-foodCont.className = 'life__necessitiesCont__foodCont';
-necessitiesCont.appendChild(foodCont);
+// let foodCont = document.createElement('div');
+// foodCont.className = 'life__necessitiesCont__foodCont';
+// necessitiesCont.appendChild(foodCont);
+let foodCont = document.getElementById('foodCont');
 
 //water container
-let waterCont = document.createElement('div');
-waterCont.className = 'life__necessitiesCont__waterCont';
-necessitiesCont.appendChild(waterCont);
+// let waterCont = document.createElement('div');
+// waterCont.className = 'life__necessitiesCont__waterCont';
+// necessitiesCont.appendChild(waterCont);
+let waterCont = document.getElementById('waterCont');
 
 //food container
-let energyCont = document.createElement('div');
-energyCont.className = 'life__necessitiesCont__energyCont';
-necessitiesCont.appendChild(energyCont);
+// let energyCont = document.createElement('div');
+// energyCont.className = 'life__necessitiesCont__energyCont';
+// necessitiesCont.appendChild(energyCont);
+let energyCont = document.getElementById('energyCont');
 
-//////add inventory items//////
-inventory.innerHTML = `
-<img id="lightning" class="inventory__item" src="${DATA.necessities.energy}" alt="person-state-img">
-<img id="bottle" class="inventory__item" src="${DATA.necessities.water}" alt="person-state-img">
-<img id="apple" class="inventory__item" src="${DATA.necessities.food}" alt="person-state-img">
+//////add inventory itemsCont items//////
+itemsCont.innerHTML = `
+<img id="lightning" class="inventory__itemsCont__item" src="${DATA.necessities.energy}" alt="person-state-img">
+<img id="bottle" class="inventory__itemsCont__item" src="${DATA.necessities.water}" alt="person-state-img">
+<img id="apple" class="inventory__itemsCont__item" src="${DATA.necessities.food}" alt="person-state-img">
 `;
 
 //////NECESSITIES strings//////
@@ -67,7 +76,7 @@ let waterString = `
 `;
 //energy string to push later on energyArr
 let energyString = `
-<img class="life__necessitiesCont__energyCont__img" src="${DATA.necessities.energy}" alt="person-state-img">
+<img class="life__necessitiesCont__waterCont__img" src="${DATA.necessities.energy}" alt="person-state-img">
 `;
 
 let foodArr = [];
@@ -84,56 +93,60 @@ let invBottle = document.getElementById('bottle');
 let invApple = document.getElementById('apple');
 
 ////// NECESSITIES functions//////
-//////add inventory items to life necessities//////
+///add default emoji (arrays lenght = null)
+personState.src = DATA.person.dead;
+///add inventory items to life necessities///
 inventory.addEventListener('click', (e)=>{
     if(e.target.id === 'lightning' && energyArr.length < 5){
         energyArr.push(energyString);
-        energyCont.innerHTML = energyArr;
+        energyCont.innerHTML = energyArr.join('');
     };
     if(e.target.id === 'bottle' && waterArr.length < 5){
         waterArr.push(waterString);
-        waterCont.innerHTML = waterArr;
+        waterCont.innerHTML = waterArr.join('');
     };
     if(e.target.id === 'apple' && foodArr.length < 5){
         foodArr.push(foodString);
-        foodCont.innerHTML = foodArr;
+        foodCont.innerHTML = foodArr.join('');
     };
+    //////person states//////
+    //FULL LIVING!!!
+    if(energyArr.length + waterArr.length + foodArr.length == 15){
+        personState.src = DATA.person.fullLiving;
+    }
+
+    //happy
+    if(energyArr.length + waterArr.length + foodArr.length > 9 && energyArr.length + waterArr.length + foodArr.length < 14){
+        personState.src = DATA.person.happy;
+    }
+
+    //neutral
+    if(energyArr.length + waterArr.length + foodArr.length <= 9){
+        personState.src = DATA.person.neutral;
+    }
+
+    //hungry
+    if(foodArr.length <= 1){
+        personState.src = DATA.person.hungry;
+    }
+
+    //thirsty
+    if(waterArr.length <= 1){
+        personState.src = DATA.person.thirsty;
+    }
+
+    //sleep
+    if(energyArr.length <= 1){
+        personState.src = DATA.person.sleep;
+    }
+
+    // //sad
+    if(energyArr.length + waterArr.length + foodArr.length <= 3){
+        personState.src = DATA.person.sad;
+    }
+
+    //dead
+    if(energyArr.length + waterArr.length + foodArr.length <= 2 || energyArr.length == 0 || waterArr.length == 0 || foodArr.length == 0){
+        personState.src = DATA.person.dead;
+    }
 });
-
-//////PERSON strings//////
-const happyString = `<img class="life__personCont__img" src="${DATA.person.happy}" alt="person-state-img"></img>`;
-const neutralString = `<img class="life__personCont__img" src="${DATA.person.neutral}" alt="person-state-img"></img>`;
-const sadString = `<img class="life__personCont__img" src="${DATA.person.sad}" alt="person-state-img"></img>`;
-const hungryString = `<img class="life__personCont__img" src="${DATA.person.hungry}" alt="person-state-img"></img>`;
-const thirstyString = `<img class="life__personCont__img" src="${DATA.person.thirsty}" alt="person-state-img"></img>`;
-const sleepString = `<img class="life__personCont__img" src="${DATA.person.sleep}" alt="person-state-img"></img>`;
-
-//////PERSON functions//////
-//happy
-// if(energyArr.length === 5 && waterArr.length === 5 && foodArr.length === 5){
-//     personCont.innerHTML = happyString;
-// }
-//neutral
-// if(energyArr.length === 5 && waterArr.length === 5 && foodArr.length === 5){
-//     personCont.innerHTML = happyString;
-// }
-
-// //sad
-// if(energyArr.length === 5 && waterArr.length === 5 && foodArr.length === 5){
-//     personCont.innerHTML = happyString;
-// }
-
-// //hungry
-// if(energyArr.length === 5 && waterArr.length === 5 && foodArr.length === 5){
-//     personCont.innerHTML = happyString;
-// }
-
-// //thirsty
-// if(energyArr.length === 5 && waterArr.length === 5 && foodArr.length === 5){
-//     personCont.innerHTML = happyString;
-// }
-
-// //sleep
-// if(energyArr.length === 5 && waterArr.length === 5 && foodArr.length === 5){
-//     personCont.innerHTML = happyString;
-// }
